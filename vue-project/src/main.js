d90297f87e5d0d6d53f7ff392c59b4ac8c183d14
JunxiC <chenjon09@gmail.com>
2023-03-20 10:16:22 -0400

    commits

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -2,9 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-Vue.config.productionTip = false
+import './assets/main.css'
 
-new Vue({
-  router,
-  render: (h) => h(App)
-}).$mount('app')
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,9 +1,25 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div class="home">
+    <h1 v-if="graduated">{{ student }}</h1>
+    <h v-else></h</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {},
+  data() {
+    return {
+      student: 'Harry',
+      graduated: false
+    }
+  }
+}
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style>
+h1 {
+  color: red;
+}
+</style>
